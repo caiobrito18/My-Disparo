24cd9ee0dd6e325a25e125d5f590cfa7317d802f
Caio de Paula Brito <caiopbrito2005@gmail.com>
2022-08-24 18:34:27 -0300

    :sparkles:/adicionando-leitura-de-csv

--- src/pages/Disparo.tsx
+++ src/pages/Disparo.tsx
@@ -1,16 +1,19 @@
-import React from 'react'
-import { FormEvent, useEffect, useState } from 'react'
-import { FileUploader } from 'react-drag-drop-files'
-import axios from 'axios'
-import '../css/App.css'
-import api from '../services/api'
-import { useNavigate } from 'react-router-dom'
-import Instancias from './Instancias'
+import React, { ChangeEvent } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
+import { FileUploader } from 'react-drag-drop-files';
+import * as XLSX from 'xlsx';
+import axios from 'axios';
+import DataTable from 'react-data-table-component';
+import '../css/App.css';
+import api from '../services/api';
+import { useNavigate } from 'react-router-dom';
 
 const Disparo = () => {
   const navigate  = useNavigate()
   var numeros:any = []
   
+  const [columns, setColumns] = useState([]);
+  const [data, setData] = useState([]);
   const [url, setUrl] = useState("")
   const [instances, setInstances] = useState([])
   const [number, setNumber] = useState("")
@@ -21,9 +24,9 @@ const Disparo = () => {
   const [greetArray, setGreetArray] = useState([""])
   const [greet, setGreet] = useState("")
   const [goodbye, setGoodBye] = useState("") 
-  const [randGreet, setRandGreet] = useState("")
-  const [randGoodbye, setRandGoodBye] = useState("")
-  const [fullMessage, setFullMessage] = useState("")
+  // const [randGreet, setRandGreet] = useState("")
+  // const [randGoodbye, setRandGoodBye] = useState("")
+  // const [fullMessage, setFullMessage] = useState("")
   const [indicador, setIndicador] = useState("")
   const [listTitle, setListTitle] = useState("")
   const [listItems, setListItems] = useState("")
@@ -50,25 +53,26 @@ const Disparo = () => {
     setGoodByeArray(splitgb);
     setGreetArray(splitg);
   }
-  async function randomizer(){
 
-    var rand1 = Math.floor(Math.random() * (goodbyeArray.length - 0 ));
-    var rand2 = Math.floor(Math.random() * (greetArray.length - 0));
-    setRandGoodBye(goodbyeArray[rand1])
-    setRandGreet(greetArray[rand2])
-    setFullMessage([randGreet,messageBody,randGoodbye].join('\n'))
-    var rand = Math.floor(Math.random() * (instances.length - 0));
-    const key = instances[rand];
-    return {fullMessage,key}
-  }
+  // async function randomizer(){
+  //   setFullMessage([randGreet,messageBody,randGoodbye].join('\n'))
+  //   var rand = Math.floor(Math.random() * (instances.length - 0));
+  //   const key = instances[rand];
+  //   return {fullMessage,key}
+  // }
 
   async function handleForm(e:FormEvent){
   e.preventDefault();
   
   const splitn = number.split(';');
   splitn.forEach(async(numero)=>{
-    let mensagem = (await randomizer()).fullMessage;
-    let chave = (await randomizer()).key;
+    var rand1 = Math.floor(Math.random() * (goodbyeArray.length - 0 ));
+    var rand2 = Math.floor(Math.random() * (greetArray.length - 0));
+    var rand3 = Math.floor(Math.random() * (instances.length - 0));
+    let randGoodbye = (goodbyeArray[rand1])
+    let randGreet = greetArray[rand2]
+    let mensagem = [randGreet,messageBody,randGoodbye].join('\n')
+    let chave = instances[rand3];
     await disparo(numero,mensagem,chave);
   })
   
@@ -102,13 +106,11 @@ async function disparo(numero:string,mensagem:string,chave:string){
 
   async function handleDebug(){
     await handleMessage();
-    await randomizer();
     await axios.get(`${url}/instance/restore`).then(res=>console.log(res))
     await getInstances();
     console.log(
       {
         number,
-        fullMessage,
         indicador,
         goodbyeArray,
         greetArray,
@@ -116,6 +118,60 @@ async function disparo(numero:string,mensagem:string,chave:string){
       }
     )
   }
+    // process CSV data
+    const processData = (dataString:string) => {
+      const dataStringLines = dataString.split(/\r\n|\n/);
+      const headers = dataStringLines[0].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+      const list = [];
+      for (let i = 1; i < dataStringLines.length; i++) {
+        const row = dataStringLines[i].split(/,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/);
+        if (headers && row.length == headers.length) {
+          const obj = {};
+          for (let j = 0; j < headers.length; j++) {
+            let d = row[j];
+            if (d.length > 0) {
+              if (d[0] == '"')
+                d = d.substring(1, d.length - 1);
+              if (d[d.length - 1] == '"')
+                d = d.substring(d.length - 2, 1);
+            }
+            if (headers[j]) {
+              obj[headers[j]] = d;
+            }
+          }
+          // remove the blank rows
+          if (Object.values(obj).filter(x => x).length > 0) {
+            list.push(obj);
+          }
+        }
+      }
+      // prepare columns list from headers
+      const columns = headers.map(c => ({
+        name: c,
+        selector: c,
+      }));
+      setData(list);
+      setColumns(columns);
+      console.log(list);
+      console.log(columns);
+    }
+    // handle file upload
+    const handleFileUpload = e => {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.onload = (evt) => {
+        /* Parse data */
+        const bstr = evt.target.result;
+        const wb = XLSX.read(bstr, { type: 'binary' });
+        /* Get first worksheet */
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname];
+        /* Convert array of arrays */
+        const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+        processData(data);
+      };
+      reader.readAsBinaryString(file);
+    }
 
   return (
   <div className="App w-[100%] height-[90%] items-center justify-between">
@@ -129,6 +185,13 @@ async function disparo(numero:string,mensagem:string,chave:string){
       </div>
       <div>
         <label htmlFor="">Números para o disparo</label>
+        <input type="file" name="csv" id="csvFile" accept=".csv,.xlsx,.xls"onChange={handleFileUpload} />
+        <DataTable
+        pagination
+        highlightOnHover
+        columns={columns}
+        data={data}
+      />
       </div> {/* coloca as informações de mensagem */} {/* Corpo da Mensagem */} <input type="text" value={number} onChange={(e)=>setNumber(e.target.value)}/> {/* Saudação */} <div className='input-el'>
         <label>Saudações separadas por ; </label>
         <input type={"text"} id='lista-titulo' value={greet} onChange={(e)=>setGreet(e.target.value)}/>
